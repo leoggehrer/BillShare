253dda177f0075d09c0063896ec5267c7158f891
ggehrer <g.gehrer@htl-leonding.ac.at>
2020-01-16 10:30:24 +0100

    Added controller objects

---
+++ BillShare.Logic/Controllers/Persistence/BillShareController.cs
@@ -0,0 +1,22 @@
+﻿using BillShare.Logic.DataContext;
+using BillShare.Logic.DataContext.Db;
+
+namespace BillShare.Logic.Controllers.Persistence
+{
+    internal abstract partial class BillShareController<E, I> : GenericController<E, I>
+       where E : Entities.IdentityObject, I, Contracts.ICopyable<I>, new()
+       where I : Contracts.IIdentifiable
+    {
+        protected DbBillShareContext BillShareContext => (DbBillShareContext)Context;
+
+        protected BillShareController(IContext context)
+            : base(context)
+        {
+        }
+        protected BillShareController(ControllerObject controller)
+            : base(controller)
+        {
+
+        }
+    }
+}
---
+++ BillShare.Logic/Migrations/20200116083708_InitDb.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BillShare.Logic.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 256, nullable: true),
+                    Description = table.Column<string>(maxLength: 256, nullable: true),
+                    Currency = table.Column<string>(maxLength: 10, nullable: false),
+                    Friends = table.Column<string>(maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bill", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expense",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BillId = table.Column<int>(nullable: false),
+                    Designation = table.Column<string>(maxLength: 256, nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    Friend = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expense", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Expense_Bill_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bill",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bill_Title",
+                table: "Bill",
+                column: "Title",
+                unique: true,
+                filter: "[Title] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expense_BillId",
+                table: "Expense",
+                column: "BillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expense");
+
+            migrationBuilder.DropTable(
+                name: "Bill");
+        }
+    }
+}
